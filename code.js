c7c8c60c14a80ac83bf4f6d7d570d547b430fc5b
ddel641 <daniel.delpiano@gmail.com>
2021-11-25 22:26:31 -0500

    Completed code for basic console RPS

--- code.js
+++ code.js
@@ -1,7 +1,53 @@
+// Computer chooses R,P,S
 function computerPlay() {
     let options = ["Rock", "Paper", "Scissors"];
     let choice = Math.floor(Math.random()*100)%3;
     return options[choice];
 }
 
-console.log(computerPlay());
+function playRPS(player, computer) {
+    player = player.charAt(0).toUpperCase() + player.slice(1);
+    if (player == "Rock" && computer == "Paper" ||
+        player == "Scissors" && computer == "Rock" ||
+        player == "Paper" && computer == "Scissors") {
+            return `You lose! ${computer} beats ${player}`;
+        } else if (player == computer) {
+            return `It's a tie, both players chose ${player}`;
+        } else {
+            return `You win! ${player} beats ${computer}`;
+        };
+}
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    
+    while (playerWins < 3 && computerWins < 3) {
+        let outcome = playRPS(getPlay(), computerPlay());
+        if (outcome.startsWith("You win")) {
+            playerWins += 1;
+        } else if (outcome.startsWith("You lose")) {
+            computerWins += 1;
+        }
+        console.log(`${outcome}
+        
+The score is now computer: ${computerWins} to player: ${playerWins}`)
+    }
+    
+    if (playerWins == 3) {
+        return "You won the game";
+    } else {
+        return "The computer won the game";
+    }
+}
+
+function getPlay() {
+    let choice;
+    let options = ["rock", "paper", "scissors"]
+    while (!options.includes(choice)) {
+        choice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+    }
+    return choice;
+}
+
+console.log(game())
