db9cc5845ced717e47f12f172b175aae8bc24947
ddel641 <daniel.delpiano@gmail.com>
2021-11-25 21:42:57 -0500

    Add code for computer choice

--- code.js
+++ code.js
@@ -1,1 +1,7 @@
-console.log("Started")
+function computerPlay() {
+    let options = ["Rock", "Paper", "Scissors"];
+    let choice = Math.floor(Math.random()*100)%3;
+    return options[choice];
+}
+
+console.log(computerPlay());
